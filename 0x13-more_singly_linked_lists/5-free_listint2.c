2ffa33b1ab5737c33c2031a545efc3e378352318
MUROBII <usmanmurabi@gmail.com>
2023-10-25 16:35:23 +0100

    Consider NULL use-case

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	while (*head)
+	if (head)
 	{
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		while (*head)
+		{
+			next = (*head)->next;
+			free(*head);
+			*head = next;
+		}
 	}
 }
